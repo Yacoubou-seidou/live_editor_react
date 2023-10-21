fe977869e2289888b87aae5ce9d1883dcda6bd64
yacoubou-seidou <yacoseidou@gmail.com>
2023-10-21 19:07:35 +0100

    Add nav component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import MonacoEditor from './components/MonacoEditor';
+import Nav from './components/Nav';
 function App() {
   return (
     <div className="App">
       <p>Hello react</p>
+      <Nav />
       <MonacoEditor />
     </div>
   );
