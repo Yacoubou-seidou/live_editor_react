24c85445b3c4b30bd1a939b92216095f6d8f59aa
yacoubou-seidou <yacoseidou@gmail.com>
2023-10-23 19:30:17 +0100

    Update navbar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <div>
+      <li>New challenge</li>
+      <li>Users</li>
+      <li>Completed</li>
+      <li>On going</li>
+      <li>Message</li>
+      <li>Intranet</li>
+    </div>
   )
 }
 
